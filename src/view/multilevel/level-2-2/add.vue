b8712eb8565e77c4b8cb0f711cfb931d3b37b3ff
hywa <1131468749@qq.com>
2018-12-25 20:37:36 +0800

    修改

--- src/view/multilevel/level-2-2/add.vue
+++ src/view/multilevel/level-2-2/add.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <form id="add" class="form-inline">
+    <form id="add" class="form-horizontal">
       <div class="form-group col-md-12" align="center">
-        <label class="col-md-3 label_desc" >姓名：</label>
-        <input type="text" class="form-control col-md-3" placeholder="输入名称" v-bind="name" v-validate="'required|email'"
+        <label class="col-md-2 control-label">姓名：</label>
+        <div class="col-md-6">
+        <input type="text" class="form-control" placeholder="输入名称" v-bind="name" v-validate="'required|email'"
                name="name"/>
-        <label class="col-md-4 label_error">{{ errors.first('name') }}</label>
+        </div>
+          <label class="col-md-4 label_error">{{ errors.first('name') }}</label>
       </div>
     </form>
   </div>
@@ -27,18 +29,19 @@
 </script>
 
 <style scoped>
-.label_desc{
-  height: 34px;
-  padding: 6px 12px;
-  font-size: 14px;
-  line-height: 1.42857143;
-  text-align: right;
-}
-.label_error{
-  height: 34px;
-  padding: 6px 12px;
-  font-size: 14px;
-  line-height: 1.42857143;
-  text-align: left;
-}
+  .label_desc {
+    height: 34px;
+    padding: 6px 12px;
+    font-size: 14px;
+    line-height: 1.42857143;
+    text-align: right;
+  }
+
+  .label_error {
+    height: 34px;
+    padding: 6px 12px;
+    font-size: 14px;
+    line-height: 1.42857143;
+    text-align: left;
+  }
 </style>
